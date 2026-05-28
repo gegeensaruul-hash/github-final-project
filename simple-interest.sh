658bf6794a9e942d5bca38970fafa47dae0bd296
gegeensaruul-hash <gegeensaruul@gmail.com>
2026-05-28 13:41:33 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,15 @@
 
+#!/bin/bash
+# Script to calculate simple interest
+
+echo "Enter Principal amount:"
+read principal
+
+echo "Enter Rate of interest (%):"
+read rate
+
+echo "Enter Time period (years):"
+read time
+
+interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
+echo "Simple Interest = $interest"
